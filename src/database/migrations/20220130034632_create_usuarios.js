4b0cc0b7290df883bb0d62f87ff6d79724597eed
Lorena <lorena@proesc.com>
2022-01-30 09:40:34 -0300

    Ajustada migration de usuarios

--- src/database/migrations/20220130034632_create_usuarios.js
+++ src/database/migrations/20220130034632_create_usuarios.js
@@ -4,6 +4,7 @@ exports.up = knex => knex.schema.createTable('usuarios', table => {
     table.text('nome').notNullable()
     table.text('email').notNullable()
     table.text('senha').notNullable()
+    table.integer('area_id').references('areas.id').notNullable()
     table.timestamp('created_at').defaultTo(knex.fn.now())
     table.timestamp('updated_at').defaultTo(knex.fn.now())
 });
